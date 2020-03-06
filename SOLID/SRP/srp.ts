cd0157465df8d1fa02792e9480df1b6c95051d07
Diego <diego@breda.build>
2020-03-06 11:59:12 +0100

    SRP

---
+++ SOLID/SRP/srp.ts
@@ -0,0 +1,34 @@
+class Book {
+    private title: string;
+    private author: string;
+    private readonly page: Page;
+
+    constructor(title: string, author: string) {
+        this.title = title;
+        this.author = author;
+        this.page = new Page()
+    }
+
+    getCurrentPage(): Page {
+        return this.page;
+    }
+}
+
+class Page {
+    readonly data: string;
+
+    constructor() {
+        this.data = 'Page 0'
+    }
+}
+
+class PagePrinter  {
+    static printer(page: Page) {
+        console.log(page.data)
+    }
+}
+
+(function main(){
+    let book = new Book('Hola Mundo', 'Juan Diego Sierra Fernández');
+    PagePrinter.printer(book.getCurrentPage());
+})();
